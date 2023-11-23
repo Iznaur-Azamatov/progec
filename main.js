356d048f4126196dd1574e08313f6df476f1dc41
Azamatov Iznaur <azamatoviznaur@gmail.com>
2023-11-23 15:25:10 +0300

    Add your commit message

---
+++ main.js
@@ -0,0 +1,55 @@
+// let name = 'Изнаур';//тип string
+// const age = 16;//тип num
+// if (age < 15){
+//    console.log('Досуп запрещён!');
+// }else{
+//     console.log('Здраствуйте!');
+// }
+// function viewMoney(){
+//     console.log('Вот вам 10$');
+// }
+// viewMoney()
+// function getMoney(){
+//     return 'Вот вам 10$';
+// }
+// getMoney()
+// function getMoney(cash = 100) {
+//     return 'Вот вам'  
+//     {cash}$;
+     
+//   }
+  
+// function squareNumber(num){
+    
+//     console.log(num * num);
+// }
+// squareNumber(4)
+// function findMaxNumber() {
+//     let max = arguments0;
+//     for (let i = 1; i < arguments.length; i++) {
+//       if (argumentsi > max) {
+//         max = argumentsi;
+//       }
+//     }
+//     console.log(max);
+//   }
+  
+//   findMaxNumber(4, 7, 2);
+
+//   function calculateDiscriminant(a, b, c) {
+//     return b**2 - 4 * a * c;
+//   }
+// function processNumbers(num1, num2) {
+//     if (num1 % 2 === 0 && num2 % 2 === 0) {
+//       return num1 * num2;
+//     } else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+//       return num1 + num2;
+//     } else {
+//       if (num1 % 2 === 0) {
+//         return num2;
+//       } else {
+//         return num1;
+//       }
+//     }
+//   }
+  
