3ad64557fc8cb7f522fe9e6ab6d06b24f249b879
Azamatov Iznaur <azamatoviznaur@gmail.com>
2023-11-25 02:49:11 +0300

    first commit

--- main.js
+++ main.js
@@ -49,3 +49,49 @@
 //     }
 //   }
   
+
+// 20 Дз
+
+//  let arr = ['lorem', 123, true, 'ipsuM', 456];
+            // 1 пункт
+// console.log(arr.length);
+            // 2 пункт
+// console.log(arr[0], arr.pop());
+
+            // 4 пункт
+// arr.unshift('lar',95,'real')
+
+//  console.log(arr);
+
+
+
+              // 3 пункт
+// let em = ['human',142,'woman',84];
+              // 5 пункт
+// em.shift()
+// em.pop()
+// em.pop()
+// em[4] = false;
+// console.log(em);
+
+// em.shift()
+// em.unshift(1)
+// em.push('items')
+// console.log(em);
+              // 6 пункт
+// let number = [1, 3, 5, 7, 9]
+
+// let evenNambers = number.map(num => num + 1);
+
+// console.log(evenNambers); 
+
+                // 7 пункт
+// let err = [1, 2, 3, 4, 5,]
+
+// err.splice(2,1)
+
+// err[0] = 'or';
+// err[1] = 'no';
+// err[2] = 6;
+// err[3] = 7;
+// console.log(err);
