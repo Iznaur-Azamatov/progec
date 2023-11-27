be22d169574edd2812b1bb639bbfe082fc32410d
Azamatov Iznaur <azamatoviznaur@gmail.com>
2023-11-27 16:42:01 +0300

    first commit

--- main.js
+++ main.js
@@ -1,97 +1,54 @@
-// let name = 'Изнаур';//тип string
-// const age = 16;//тип num
-// if (age < 15){
-//    console.log('Досуп запрещён!');
-// }else{
-//     console.log('Здраствуйте!');
-// }
-// function viewMoney(){
-//     console.log('Вот вам 10$');
-// }
-// viewMoney()
-// function getMoney(){
-//     return 'Вот вам 10$';
-// }
-// getMoney()
-// function getMoney(cash) {
-//    console.log(`Вот вам ${cash}`);
+        // 1 пункт
+// let arr = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
 
-//   }
-//   getMoney('100$')
-// function squareNumber(num){
-    
-//     console.log(num * num);
-// }
-// function maxNumber(n1,n2,n3) {
-//  return Math.max(n1,n2,n3);
-  
-//   }
-//   console.log(maxNumber(1, 2, 3));
- 
-  
-  
-//   findMaxNumber(4, 7, 2);
-
-//   function calculateDiscriminant(a, b, c) {
-//     return b**2 - 4 * a * c;
-//   }
-// function processNumbers(num1, num2) {
-//     if (num1 % 2 === 0 && num2 % 2 === 0) {
-//       return num1 * num2;
-//     } else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
-//       return num1 + num2;
-//     } else {
-//       if (num1 % 2 === 0) {
-//         return num2;
-//       } else {
-//         return num1;
-//       }
+// arr.forEach(num => {
+//     if (num % 2 !== 0) {
+//         console.log(num);
 //     }
-//   }
+// })
+        // 2 пункт
+// let words = [ 'lorem' , 'ipsum', 'javascript','php','css' ,'react' , 'git','html','mysql']
+
+// let words2 = words.filter(function(item) {
+//     return item.length < 4;
+// })
+// console.log(words2);
+        // 3 пункт
+// let numbers =  [1, -3, 5, 6, -7, 8, 9, -11];
+// let numbers2 = numbers.filter(num => num < 0 );
+// console.log(numbers2);
+        // 4 пункт
+// let arr2 = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+
+// arr2.forEach(num => {
+//     if (num % 2 !== 0) {
+//         console.log(num);
+//     }
+// })
+        // 5 пункт
+// let words3 = [ 'lorem' , 'ipsum', 'javascript','php','css' ,'react' , 'git','html','mysql']
+
+// let words4 = words3.filter(function(item) {
+//     return item.length > 3;
+// })
+// console.log(words4);
+        // 6 пункт
+// let err =  [5, 6, 7, 8, 9];
+// let err2 = err.map(num => num ** 2);
+// console.log(err2);
+        // 7 пункт
+
+// let sum = [{a: 10, b: 5}, {a: 20, b: 22}, {a: 131, b:55}];
+// let sumArray = sum.map(obj => obj.a + obj.b);
+// console.log(sumArray); 
+        // 8 пункт
+// let nrr = [-13, 0, 12, 1662, -0.32, -102, -2];
+// let nrr2 = nrr.filter(num => num < 0 );
+// let nrr3 = nrr2.reduce((sum, current) => sum + current);
+// console.log(nrr3);
+        // 9 пункт
+// let sam =  [ {x: 10, y: 'lorem'}, {x: 21, y: 'lorem'}, {x: -17, y: 'lorem'}, {x: 156, y: 'lorem'}];
+// let sam2 = sam.map(obj => obj.x);
+// let sam3 = sam2.reduce((sum, current) => sum + current);
+//   console.log(sam3);
   
-
-// 20 Дз
-
-//  let arr = ['lorem', 123, true, 'ipsuM', 456];
-            // 1 пункт
-// console.log(arr.length);
-            // 2 пункт
-// console.log(arr[0], arr.pop());
-
-            // 4 пункт
-// arr.unshift('lar',95,'real')
-
-//  console.log(arr);
-
-
-
-              // 3 пункт
-// let em = ['human',142,'woman',84];
-              // 5 пункт
-// em.shift()
-// em.pop()
-// em.pop()
-// em[4] = false;
-// console.log(em);
-
-// em.shift()
-// em.unshift(1)
-// em.push('items')
-// console.log(em);
-              // 6 пункт
-// let number = [1, 3, 5, 7, 9]
-
-// let evenNambers = number.map(num => num + 1);
-
-// console.log(evenNambers); 
-
-                // 7 пункт
-// let err = [1, 2, 3, 4, 5,]
-
-// err.splice(2,1)
-
-// err[0] = 'or';
-// err[1] = 'no';
-// err[2] = 6;
-// err[3] = 7;
-// console.log(err);
